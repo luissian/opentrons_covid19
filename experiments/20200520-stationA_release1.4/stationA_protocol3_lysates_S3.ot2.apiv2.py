e003608b088f4f3259e3ea37f0d334d0984cfff0
saramonzon <sara.monzon.fdz@gmail.com>
2020-05-20 11:19:25 +0200

    fix default par order in run_info function

--- experiments/20200520-stationA_release1.4/stationA_protocol3_lysates_S3.ot2.apiv2.py
+++ experiments/20200520-stationA_release1.4/stationA_protocol3_lysates_S3.ot2.apiv2.py
@@ -98,7 +98,7 @@ elif LANGUAGE_DICT[LANGUAGE] == 'esp':
     }
 
 # Function definitions
-def run_info(parameters = dict(),start,end):
+def run_info(start, end, parameters = dict()):
     info = {}
     hostname = subprocess.run(
         ['hostname'],
@@ -327,4 +327,4 @@ following:\nopentrons deep generic well plate\nnest deep generic well plate\nvwr
         "LANGUAGE" : LANGUAGE,
         "RESET_TIPCOUNT" : RESET_TIPCOUNT
     }
-    run_info(par, start_time, finish_time)
+    run_info(start_time, finish_time, par)

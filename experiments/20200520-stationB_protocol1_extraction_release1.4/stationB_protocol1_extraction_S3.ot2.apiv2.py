98457d7b2ac5395253753f59380b7aa595720fa4
saramonzon <sara.monzon.fdz@gmail.com>
2020-05-20 11:56:10 +0200

    updated experiment for b station test for release 1.4

--- experiments/20200520-stationB_protocol1_extraction_release1.4/stationB_protocol1_extraction_S3.ot2.apiv2.py
+++ experiments/20200520-stationB_protocol1_extraction_release1.4/stationB_protocol1_extraction_S3.ot2.apiv2.py
@@ -34,7 +34,7 @@ REAGENT SETUP:
 # Warning writing any Parameters below this line.
 # It will be deleted if opentronsWeb is used.
 
-NUM_SAMPLES = 96
+NUM_SAMPLES = 16
 REAGENT_LABWARE = 'nest 12 reservoir plate'
 MAGPLATE_LABWARE = 'nest deep generic well plate'
 WASTE_LABWARE = 'nest 1 reservoir plate'
@@ -66,6 +66,7 @@ MAGPLATE_LABWARE must be one of the following:
     opentrons deep generic well plate
     nest deep generic well plate
     vwr deep generic well plate
+    ecogen deep generic well plate
 
 WASTE labware
     nest 1 reservoir plate
@@ -77,6 +78,16 @@ ELUTION_LABWARE
 # Parameters to adapt the protocol
 # Warning writing any Parameters below this line.
 # It will be deleted if opentronsWeb is used.
+
+# Calculated variables
+if MAGPLATE_LABWARE == 'nest deep generic well plate':
+    MAGNET_HEIGHT = 22
+elif MAGPLATE_LABWARE == 'vwr deep generic well plate':
+    MAGNET_HEIGHT = 22
+elif MAGPLATE_LABWARE == 'ecogen deep generic well plate':
+    MAGNET_HEIGHT = 21
+else:
+    MAGNET_HEIGHT = 22
 
 # End Parameters to adapt the protocol
 ACTION = "StationB-protocol1-extraction"
@@ -315,8 +326,8 @@ def wash(wash_sets,dests,waste,magdeck,pip,tiprack):
             pip.mix(7, 200, m.bottom(2))
             pip.flow_rate.dispense = dispense_default_speed
 
-            magdeck.engage(height_from_base=22)
-            robot.delay(seconds=75, msg='Incubating on magnet for 75 seconds.')
+            magdeck.engage(height_from_base=MAGNET_HEIGHT)
+            #robot.delay(seconds=75, msg='Incubating on magnet for 75 seconds.')
 
             # remove supernatant
             aspire_default_speed = pip.flow_rate.aspirate
@@ -339,9 +350,9 @@ def elute_samples(sources,dests,buffer,magdeck,pip,tipracks):
         drop(pip)
 
     ## Incubation steps
-    robot.delay(minutes=5, msg='Incubating off magnet for 5 minutes.')
-    magdeck.engage(height_from_base=22)
-    robot.delay(seconds=120, msg='Incubating on magnet for 120 seconds.')
+    #robot.delay(minutes=5, msg='Incubating off magnet for 5 minutes.')
+    magdeck.engage(height_from_base=MAGNET_HEIGHT)
+    #robot.delay(seconds=120, msg='Incubating on magnet for 120 seconds.')
 
     aspire_default_speed = pip.flow_rate.aspirate
     pip.flow_rate.aspirate = 50
@@ -461,11 +472,11 @@ following:\nopentrons deep generic well plate\nnest deep generic well plate\nvwr
         mix_beads(7, mag_samples_m,m300,tips300)
 
     # incubate off the magnet
-    robot.delay(minutes=10, msg='Incubating off magnet for 10 minutes.')
+    #robot.delay(minutes=10, msg='Incubating off magnet for 10 minutes.')
 
     ## First incubate on magnet.
-    magdeck.engage(height_from_base=22)
-    robot.delay(minutes=7, msg='Incubating on magnet for 7 minutes.')
+    magdeck.engage(height_from_base=MAGNET_HEIGHT)
+    #robot.delay(minutes=7, msg='Incubating on magnet for 7 minutes.')
 
     # remove supernatant with P1000
     remove_supernatant(mag_samples_s,waste,p1000,tips1000)

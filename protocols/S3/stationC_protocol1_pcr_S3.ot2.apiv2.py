13447135adbdf8b930328c3c1bbc14623c7adfc9
MiguelJulia <migueljuliamolina@gmail.com>
2020-05-26 13:09:38 +0200

    Always drop tip after mixing mm

--- protocols/S3/stationC_protocol1_pcr_S3.ot2.apiv2.py
+++ protocols/S3/stationC_protocol1_pcr_S3.ot2.apiv2.py
@@ -349,8 +349,7 @@ def homogenize_mm(mm_tube, pip, tiprack, times=5):
         pip.dispense(200, mm_tube.bottom(volume_height))
     # blow out before dropping tip
     pip.blow_out(mm_tube.top(-2))
-    if VOLUME_MMIX < 20:
-        drop(pip)
+    drop(pip)
 
 def prepare_mastermix(mm_rack, p300, p20,tiprack300,tiprack20):
     # setup mastermix coordinates
